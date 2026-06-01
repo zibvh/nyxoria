6f2d07ab6ebab77ada0e78e7a67d3b4b7e670754
zibvh <bhuszibah@gmail.com>
2026-06-01 08:30:21 +0100

    new

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const MONGO_URI = 'mongodb+srv://nyxoria:nyxoria@midnight.hxjptxk.mongodb.net/nyxoria?appName=Midnight';
+const MONGO_URI = 'mongodb+srv://Nyxoria:nyxoria@midnight.hxjptxk.mongodb.net/nyxoria?appName=Midnight';
 
 // ── Middleware ──────────────────────────────────────────────────────────────
 app.use(cors());
